8f219592225ddbf28db100d618ca003aa60b2f58
Jyoti Ahuja <jyotiahuja822@gmail.com>
2024-11-14 20:12:00 +0530

    initial commit

---
+++ api/index.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+const cors = require("cors")
+
+const express = require('express')
+const app = express()
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true 
+}))
+
+app.use(express.json())
+
+const apiData = require('../dummyData')
+// app.set("view engine","ejs") //have to verify this
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        app.listen(
+            port , ()=>{ console.log(`The server is listening to ${port}`)}
+        )
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to Attyre backend")
+})
+
+app.post('/generate-palette',(req,res)=>{
+    const {skin , eye , hair , colorIntensityPreference ,
+        seasonalPreference,
+        occasionPreference,
+        personalPreference,
+        undertone
+    } = req.body
+
+    console.log('in generate palette ' , req.body )
+    res.status(200).json({
+        status : 'success',
+        palette:apiData
+    })
+})
+
+module.exports = app;
